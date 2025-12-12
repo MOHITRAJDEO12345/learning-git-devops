75c5d0415b0581dc93da0a4a5807489f10eed6d2
mohitkingarthursaber <mohitkingarthursaber@gmail.com>
2025-12-12 13:08:05 +0530

    added divide function

--- calculator.py
+++ calculator.py
@@ -49,10 +49,10 @@ def subtract(a, b):
     return a - b
 
 
-# def divide(a, b):
-#     if b == 0:
-#         raise ZeroDivisionError("Cannot divide by zero.")
-#     return a / b
+def divide(a, b):
+    if b == 0:
+        raise ZeroDivisionError("Cannot divide by zero.")
+    return a / b
 
 
 def main():
